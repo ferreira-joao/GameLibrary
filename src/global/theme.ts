7ca65f7a7084e59aff1157c594a8fd81809b88f8
ferreira-joao <klausblack231@gmail.com>
2022-05-13 10:29:44 -0300

    initial config

---
+++ src/global/theme.ts
@@ -0,0 +1,24 @@
+const size = {
+  xxs: "320px",
+  xs: "375px",
+  sm: "425px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1440px",
+  xxl: "2560px",
+};
+
+export const breakpoint = {
+  xxs: `(min-widht: ${size.xxs})`,
+  xs: `(min-width: ${size.xs})`,
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+  xxl: `(min-width: ${size.xxl})`,
+};
+
+export const colors = {
+  primary: "#be276e",
+  background: "#000033",
+};
---
+++ src/pages/Home/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import { colors } from "../../global/theme";
+
+export const Container = styled.div`
+  background-color: ${colors.background};
+
+  h1 {
+    color: #fff;
+  }
+`;
