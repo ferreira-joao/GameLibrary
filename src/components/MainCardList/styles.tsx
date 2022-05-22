09be721ea4cfba586154678c6a693a5e801ae434
Nome Sobrenome <joaovfsc231@gmail.com>
2022-05-21 23:15:05 -0300

    added grid to MainCardList

--- src/components/MainCardList/styles.tsx
+++ src/components/MainCardList/styles.tsx
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const List = styled.ul``;
+import { breakpoint } from "../../global/theme";
+
+export const List = styled.ul`
+  @media ${breakpoint.md} {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 20px;
+  }
+`;
