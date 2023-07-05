0f6a9c435cc4633b3a2b3e294c237d82e76ed7f0
ferreira-joao <joaovfsc231@gmail.com>
2023-07-05 08:26:14 -0300

    :construction: Added new api call to get game details

--- src/services/apiCalls.tsx
+++ src/services/apiCalls.tsx
@@ -10,3 +10,12 @@ export const getGames = (search: string, size: number) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const getGameDetails = (id: number) => {
+  return api
+    .get(`/api/games/${id}?key=${env}`)
+    .then((res) => {
+      return res.data.results;
+    })
+    .catch((err) => console.log(err));
+};
